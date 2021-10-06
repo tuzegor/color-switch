61e1d010afcda1e63c39dc7c1ce407d2ae4bc490
Yehor Tuz <tuzegor@gmail.com>
2021-10-06 23:39:16 +0300

    тестим

---
+++ js/colorChange.js
@@ -0,0 +1,4 @@
+console.log("hello");
+import refs from './refs'
+const {start, stop} = refs;
+console.log(start);
---
+++ js/refs.js
@@ -0,0 +1,4 @@
+export default {
+    start: document.querySelector('[data-action="start"]'),
+    stop: document.querySelector('[data-action="stop"]'),
+}
