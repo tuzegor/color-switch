ec68a19b301fbf4c4465f075f78277aa03fe41a1
Yehor Tuz <tuzegor@gmail.com>
2021-10-08 00:40:34 +0300

    done

--- js/colorChange.js
+++ js/colorChange.js
@@ -1,4 +1,37 @@
 
 import refs from './refs.js'
-const {start, stop} = refs;
-console.log(start);
+const {start, stop, body} = refs;
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+let intID = null;
+  
+start.addEventListener('click', startChangeColor)
+stop.addEventListener('click', stopChangeColor)
+
+function startChangeColor () {
+    intID = setInterval(()=>{
+        body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length-1)];
+        console.log(body.style.backgroundColor);
+        start.disabled = true
+    },1000)
+
+
+}
+
+function stopChangeColor () {
+    clearInterval(intID)
+    start.disabled = false
+}
+
--- js/refs.js
+++ js/refs.js
@@ -1,4 +1,5 @@
 export default {
+    body: document.querySelector('body'),
     start: document.querySelector('[data-action="start"]'),
     stop: document.querySelector('[data-action="stop"]'),
 }
